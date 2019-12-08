1026dfe281bbf7bcc1b88b1027e96ce57696ec3f
Peng Gu <36422171+P-Gu@users.noreply.github.com>
2019-12-08 13:38:36 -0500

    Add files via upload

--- predict.py
+++ predict.py
@@ -8,18 +8,19 @@ from keras import regularizers
 from keras.models import load_model
 import numpy as np
 import os, sys
+import csv
 
 import matplotlib.image as mpimg
 
 from PIL import Image
 
-model = load_model("my_model.h5")
+model = load_model("my_model2.h5")
 X=[]
 Y=[]
 
-for root, dirs, files in os.walk("Data", topdown=True):
+for root, dirs, files in os.walk("Dec7DataModified", topdown=True):
    for file in files:
-      if os.path.basename(root)[-1] == "w":
+      if os.path.basename(root)[-1] == "a" and len(os.path.basename(root))==1:
           label = os.path.basename(root)[-1]
           img = Image.open(os.path.join(root,file))
           img = img.resize((227,227))
@@ -28,10 +29,25 @@ for root, dirs, files in os.walk("Data", topdown=True):
           img = (img / 127.5) - 1
           X.append(img)
           Y.append(label)
-   if os.path.basename(root)[-1] == "w": break
+   if os.path.basename(root)[-1] == "a" and len(os.path.basename(root))==1: break
 
 X = np.asarray(X)
 #im = np.expand_dims(img, axis=0)
 y_prob = model.predict(X)
-print(y_prob)
+print(y_prob[:,0])
+
+labelList = ["d", "w", "a"]
+
+Y_prob = []
+
+for i in range(y_prob.shape[0]):
+    row = y_prob[i, : ]
+    index = np.argmax(row)
+    label = labelList[index]
+    Y_prob.append(label)
+
 print(Y)
+
+with open('result.csv', 'w') as myfile:
+    wr = csv.writer(myfile, dialect='excel')
+    wr.writerows(Y_prob)
